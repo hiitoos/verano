9fb46cc66e0cd9f8c4b220d4ae025232e2f63c85
hiitoos <adrianhitos@gmail.com>
2020-08-31 13:15:59 +0200

    Submenus added

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
 
 public class main {
-    static Scanner scan = new Scanner (System.in);
+    static Scanner scanInt = new Scanner (System.in);
+    static Scanner scanSt = new Scanner (System.in);
+
     static int n;
     public static void main(String[] args) {
         do {
             System.out.println("Selecciona una opción:\n1- Listar tarea\n2.- Cambiar estado\n3.- Editar tarea\n4.- Borrar tarea\n5.- Búsqueda\n0.- Salir.");
-            n=scan.nextInt();
+            n= scanInt.nextInt();
             switch (n){
                 case 1 -> listarTareas();
                 case 2 -> cambiarEstado();
@@ -32,7 +34,7 @@ public class main {
         //Editar una Tarea (Solo Título, Descripción o Responsable)
         do{
             System.out.println("¿Qué quieres editar?\n1.- Título\n2.- Descripción\n3.- Responsable\n0.- Salir");
-            n=scan.nextInt();
+            n= scanInt.nextInt();
             switch (n){
                 case 1 -> editTitulo();
                 case 2 -> editDescripcion();
@@ -48,6 +50,16 @@ public class main {
     }
 
     public static void busqueda(){
+        System.out.println("Selecciona una opción\n1.- Búsqueda de usuario\n2.- Búsqueda por nombre");
+        n = scanInt.nextInt();
+        System.out.println("Introduce texto a buscar");
+
+        if (n==1) buscarUsuario(scanSt.nextLine());
+        else{
+            if (n==2) buscarNombre(scanSt.nextLine());
+            else System.out.println("Opción incorrecta");
+        }
+
         //Búsqueda por: Usuario o Coincidencia de Nombre.
     }
 
@@ -56,5 +68,9 @@ public class main {
     public static void editTitulo(){}
     public static void editDescripcion(){}
     public static void editResponsable(){}
+
+    //Funciones busqueda
+    public static void buscarUsuario(String usuario){}
+    public static void buscarNombre(String usuario){}
 
 }
