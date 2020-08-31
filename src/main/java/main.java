9f8f22795c37d29ed6fb866734e16b020460d94b
hiitoos <67627517+hiitoos@users.noreply.github.com>
2020-08-31 10:52:21 +0200

    addedMenu

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,28 +1,25 @@
 import java.util.Scanner;
 
 public class main {
+    static Scanner scan = new Scanner (System.in);
+    static int n;
     public static void main(String[] args) {
-        Scanner scan = new Scanner (System.in);
-        int n=0;
         do {
-            System.out.println("Selecciona una opción");
-            switch (scan.nextInt()){
+            System.out.println("Selecciona una opción:\n1- Listar tarea\n2.- Cambiar estado\n3.- Editar tarea\n4.- Borrar tarea\n5.- Búsqueda\n0.- Salir.");
+            n=scan.nextInt();
+            switch (n){
                 case 1 -> listarTareas();
                 case 2 -> cambiarEstado();
                 case 3 -> editarTarea();
                 case 4 -> borrarTarea();
                 case 5 -> busqueda();
-
-                        /*Crear una Nueva Tarea
-                        Cambiar de Estado de una Tarea
-                        Editar una Tarea (Solo Título, Descripción o Responsable)
-                        Borrar una Tarea
-                        Búsqueda por: Usuario o Coincidencia de Nombre.
-                        */
+                case 0 -> System.exit(0);
+                default -> System.out.println("Selección errónea");
             }
         }while (n!=0);
     }
 
+    //Funciones menú principal
     public static void listarTareas(){
 
     }
@@ -32,7 +29,18 @@ public class main {
     }
 
     public static void editarTarea(){
-
+        //Editar una Tarea (Solo Título, Descripción o Responsable)
+        do{
+            System.out.println("¿Qué quieres editar?\n1.- Título\n2.- Descripción\n3.- Responsable\n0.- Salir");
+            n=scan.nextInt();
+            switch (n){
+                case 1 -> editTitulo();
+                case 2 -> editDescripcion();
+                case 3 -> editResponsable();
+                case 0 -> System.exit(0);
+                default -> System.out.println("Selección errónea");
+            }
+        }while (n!=0);
     }
 
     public static void borrarTarea(){
@@ -40,7 +48,13 @@ public class main {
     }
 
     public static void busqueda(){
-
+        //Búsqueda por: Usuario o Coincidencia de Nombre.
     }
+
+
+    //Funciones editar
+    public static void editTitulo(){}
+    public static void editDescripcion(){}
+    public static void editResponsable(){}
 
 }
