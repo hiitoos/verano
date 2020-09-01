0f03d5c257aebe086d01f1789298054a5e90bc84
hiitoos <adrianhitos@gmail.com>
2020-09-01 16:19:35 +0200

    Now is bonico

--- src/main/java/com/main/Hibernate.java
+++ src/main/java/com/main/Hibernate.java
@@ -23,9 +23,15 @@ public class Hibernate {
 
     public static void showT() {
         List<Tareas> tareas = (List<Tareas>) em.createQuery("FROM Tareas").getResultList();
-        Iterator iter = tareas.iterator();
-        while (iter.hasNext())
-            System.out.println(iter.next());
+        for(int i=0; i<tareas.size(); i++){
+            System.out.println("ID: " + tareas.get(i).getId());
+            System.out.println("Titulo: " + tareas.get(i).getTitulo());
+            System.out.println("Descripcion: " + tareas.get(i).getDescripcion());
+            System.out.println("Fecha Creacion: " + tareas.get(i).getFechaCreacion());
+            System.out.println("Responsable: " + tareas.get(i).getResponsable());
+            System.out.println("Estado: " + tareas.get(i).getEstado());
+            System.out.println("--------------------------------------------------------------------");
+        }
     }
 
     public static List<Tareas> getTearea(){
@@ -35,15 +41,20 @@ public class Hibernate {
 
     public static void showUser() {
         List<Usuarios> users = (List<Usuarios>) em.createQuery("FROM Usuarios").getResultList();
-        Iterator iter = users.iterator();
-        while (iter.hasNext())
-            System.out.println(iter.next());
+        for(int i=0; i<users.size(); i++){
+            System.out.println("ID: " + users.get(i).getId());
+            System.out.println("Nombre: " + users.get(i).getNombre());
+            System.out.println("Apellidos: " + users.get(i).getAppellidos());
+            System.out.println("Email: " + users.get(i).getEmail());
+            System.out.println("--------------------------------------------------------------------");
+        }
     }
 
     public static void updateTarea(Tareas tarea) {
         em.getTransaction().begin();
         em.merge(tarea);
         em.getTransaction().commit();
+        System.out.println("¡¡Tarea actualizada!!");
     }
 
     public static void deleteTarea(int id) {
@@ -51,21 +62,31 @@ public class Hibernate {
         em.getTransaction().begin();
         em.remove(tarea);
         em.getTransaction().commit();
+        System.out.println("¡¡Tarea eliminada!!");
     }
 
     public static void readEstado() {
         List<Estados> estados = (List<Estados>) em.createQuery("FROM Estados").getResultList();
-        Iterator iter = estados.iterator();
-        while (iter.hasNext())
-            System.out.println(iter.next());
+        for(int i=0; i<estados.size(); i++){
+            System.out.println("ID: " + estados.get(i).getId());
+            System.out.println("Nombre: " + estados.get(i).getNombre());
+            System.out.println("Descripcion: " + estados.get(i).getDescripcion());
+            System.out.println("--------------------------------------------------------------------");
+        }
     }
 
     public static void buscarUsuario(String usuario) {
         List id = em.createQuery("select id FROM Usuarios WHERE nombre LIKE '%"+usuario+"%'").getResultList();
         List<Tareas> tareas = (List<Tareas>) em.createQuery("FROM Tareas WHERE responsable = " + id.get(0)).getResultList();
-        Iterator iter = tareas.iterator();
-        while (iter.hasNext())
-            System.out.println(iter.next());
+        for(int i=0; i<tareas.size(); i++){
+            System.out.println("ID: " + tareas.get(i).getId());
+            System.out.println("Titulo: " + tareas.get(i).getTitulo());
+            System.out.println("Descripcion: " + tareas.get(i).getDescripcion());
+            System.out.println("Fecha Creacion: " + tareas.get(i).getFechaCreacion());
+            System.out.println("Responsable: " + tareas.get(i).getResponsable());
+            System.out.println("Estado: " + tareas.get(i).getEstado());
+            System.out.println("--------------------------------------------------------------------");
+        }
     }
 
     public static void close() {
